23f6d74a7a9221bdaba20b2091b55818ac7d6524
Hd8Dev <tymekradeksikorski1@gmail.com>
2024-11-08 20:05:58 +0100

    main.lua

--- main.lua
+++ main.lua
@@ -1,17 +1,79 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Player = Player.LocalPlayer.Name
+	print(Player)
+local work.1 = workspace
+local 1.c = "O. Ome"
+local 1.g = "G. One"
 
-local Window = Library.CreateLib("JoLo Hub | Alpha", "DarkTheme")
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/Hd8Dev/hdhub-beta/refs/heads/main/main.lua")))()
 
-local MainTab = Window:NewTab("Main | Alpha")
+if Player ~= Player.LocalPlayer then
+	print("Error... <404>")
+else
+	print("Loading... <101>")
+		end
+			local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))() -- Botting the orion...
 
-local MiscTab = Window:NewTab("Misc | Pre-realese")
+local Window = OrionLib:MakeWindow({Name = "Main. HDHub | Key", HidePremium = false, SaveConfig = true, ConfigFolder = "key-m23Jv-hdhub=mainkey^4", IntroText = "Key System | HD Hub"})
 
-local HomeTab = Window:NewTab("Home | git.org/jolohub/tests/hometab")
 
-local SectionTroll = MainTab:NewSection("<404>")
 
-local MiscSection1 = MiscTab:NewSection("Home | <C. Menu>")
+local HomeTab = Window:MakeTab({
+	Name = "G. Home | Info",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
 
-MiscSection:NewTextBox("Credits", "hd8 ", function(txt)
-	print(txt)
-end)
+
+OrionLib:MakeNotification({
+	Name = "HD Hub | Notification",
+	Content = "! Disclaimer ! This isnt the script, it's the key system  | HD Hub Info",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+
+
+--[[
+Note; 
+This is the beta test of the key system
+Were not using ads you just need to click a button to generate key / we dont use ads !
+// THIS ISNT A COPY OF JOLO HUB!
+// IM THE OWNER! 
+// HD COMMUNITY IS THE DEVS TEAM!
+// EVERYTHING IS COPYRIGHTED!
+		--]]
+
+Tab:AddParagraph("Paragraph","Paragraph Content")
+
+
+
+
+
+HomeTab:AddButton({
+	Name = "Get Key | Key at console",
+	Callback = function()
+print("PremiumKeyY")
+			
+  	end    
+})
+
+
+
+
+Tab:AddTextbox({
+	Name = "Put in key | Key System",
+	Default = "nil",
+	TextDisappear = true,
+	Callback = function(KeySys)
+		if KeySys ~= "PremiumKeyY"
+				print('key wrong...')
+
+			else
+				print('Loading... <404>')
+	end	  
+})
+
+
+
+	
+
+
