e15a49e122cae754620156488b30a2412f97e7f4
Hd8Dev <tymekradeksikorski1@gmail.com>
2024-11-08 20:26:15 +0100

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet(("")))()
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/Hd8Dev/hdhub-beta/refs/heads/hdhub/main.lua")))()
+      loadstring(game:HttpGet(("")))()
